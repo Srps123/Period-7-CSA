656e1178e25bf20b7fecd30119772c731c3e04fa
Srps123 <107730698+Srps123@users.noreply.github.com>
2022-08-23 14:46:32 -0700

    Add files via upload

---
+++ GreenfootSeatingChartMaster2022-23/SreenidhiSreenivasan.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SreenidhiSreenivasan here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class SreenidhiSreenivasan extends Student
+{
+    /**
+     * Act - do whatever the SreenidhiSreenivasan wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
